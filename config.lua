e32ee2ed19ca0d0d7473b076ac7feafec6cfa7cb
Frankie McGough <fmcgough@live.co.uk>
2021-09-13 09:03:19 +0100

    More lvim config updates

--- config.lua
+++ config.lua
@@ -1,19 +1,25 @@
 -- THESE ARE EXAMPLE CONFIGS FEEL FREE TO CHANGE TO WHATEVER YOU WANT
+-- vim:fdm=marker:
 
--- general
+-- general {{{
 lvim.format_on_save = false
 lvim.lint_on_save = false
 vim.opt.background = "light"
 vim.opt.colorcolumn = "120"
+-- }}}
 
 -- colour scheme config in Lua
+local colorscheme = "pencil-color" -- "neon", "material", "tokyonight", "github", "PaperColor"
+-- tokyonight {{{
 vim.g.tokyonight_style = "day"
 vim.g.tokyonight_italic_functions = false
 vim.g.tokyonight_sidebars = { "qf", "vista_kind", "terminal", "packer", "NvimTree" }
 vim.g.tokyonight_day_brightness = 0.4
 vim.g.neon_bold = true
 vim.g.neon_style = "light"
+-- }}}
 
+-- neon {{{
 -- default:
 -- diff_red = "#fb4934",
 -- diff_green = "#8ec07c",
@@ -52,26 +58,73 @@ vim.g.neon_overrides = {
   GitSignsDelete = { fg = diff_red },
   GitSignsDeleteNr = { fg = diff_red },
   GitSignsDeleteLn = { fg = diff_red },
-
   SignifySignAdd = { fg = diff_green },
   SignifySignChange = { fg = diff_yellow },
   SignifySignDelete = { fg = diff_red },
 }
+-- }}}
 
+-- edge {{{
 vim.g.edge_style = "light"
+-- }}}
 
+-- material {{{
 vim.g.material_style = "lighter"
 vim.g.material_lighter_contrast = true
 vim.g.material_borders = true
 vim.g.material_italic_comments = true
+-- }}}
 
--- Load the colorscheme
-lvim.colorscheme = "tokyonight" -- "neon", "material", "tokyonight", "github", "PaperColor"
+-- PaperColor {{{
+vim.g.PaperColor_Theme_Options = {
+  theme = {
+    default = {
+      allow_italic = 1,
+    },
+    ["default.light"] = {
+      override = {
+        linenumber_bg = { "#dadada", "253" },
+      },
+    },
+  },
+}
 
--- keymappings [view all the defaults by pressing <leader>Lk]
+-- pencil statusline colours:
+-- let s:base03  = {'t': s:ansi_colors ?   8 : (s:tty ? '0' : 234), 'g': '#212121'}
+--   let s:base02  = {'t': s:ansi_colors ? '0' : (s:tty ? '0' : 235), 'g': '#424242'}
+--   let s:base01  = {'t': s:ansi_colors ?  10 : (s:tty ? '0' : 240), 'g': '#909090'}
+--   let s:base00  = {'t': s:ansi_colors ?  11 : (s:tty ? '7' : 241), 'g': '#545454'}
+--   let s:base0   = {'t': s:ansi_colors ?  12 : (s:tty ? '7' : 244), 'g': '#B2B2B2'}
+--   let s:base1   = {'t': s:ansi_colors ?  14 : (s:tty ? '7' : 245), 'g': '#636363'}
+--   let s:base2   = {'t': s:ansi_colors ?   7 : (s:tty ? '7' : 254), 'g': '#D9D9D9'}
+--   let s:base3   = {'t': s:ansi_colors ?  15 : (s:tty ? '7' : 7  ), 'g': '#C6C6C6'}
+--   let s:darkblue= {'t': s:ansi_colors ?   4 : (s:tty ? '4' : 24 ), 'g': '#005F87'}
+--   let s:orange  = {'t': s:ansi_colors ?   9 : (s:tty ? '1' : 166), 'g': '#D75F5F'}
+--   let s:red     = {'t': s:ansi_colors ?   1 : (s:tty ? '1' : 160), 'g': '#C30771'}
+--   let s:magenta = {'t': s:ansi_colors ?   5 : (s:tty ? '5' : 125), 'g': '#E32791'}
+--   let s:violet  = {'t: s:ansi_colors ?  13 : (s:tty ? '5' : 61 ), 'g': '#6855DE'}
+--   let s:blue    = {'t': s:ansi_colors ?   4 : (s:tty ? '4' : 33 ), 'g': '#008EC4'}
+--   let s:cyan    = {'t': s:ansi_colors ?   6 : (s:tty ? '6' : 37 ), 'g': '#20A5BA'}
+--   let s:green   = {'t': s:ansi_colors ?   2 : (s:tty ? '2' : 64 ), 'g': '#10A778'}
+
+
+if colorscheme == "PaperColor" then
+  vim.cmd [[ highlight SignColumn ctermbg=253 guibg=#dadada ]]
+  vim.cmd [[ highlight! link SignifySignAdd             DiffAdd]]
+  vim.cmd [[ highlight! link SignifySignChange          DiffChange ]]
+  vim.cmd [[ highlight! link SignifySignDelete          DiffDelete ]]
+  vim.cmd [[ highlight! link SignifySignChangeDelete    SignifySignChange ]]
+  vim.cmd [[ highlight! link SignifySignDeleteFirstLine SignifySignDelete ]]
+end
+
+-- }}}
+
+-- keymappings {{{
+-- view all the defaults by pressing <leader>Lk
 lvim.leader = "space"
 -- add your own keymapping
 lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
+lvim.keys.normal_mode["<CR>"] = ":"
 -- unmap a default keymapping
 -- lvim.keys.normal_mode["<C-Up>"] = ""
 -- edit a default keymapping
@@ -107,33 +160,81 @@ lvim.builtin.which_key.mappings["t"] = {
   q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
   l = { "<cmd>Trouble loclist<cr>", "LocationList" },
 }
+-- }}}
 
--- TODO: User Config for predefined plugins
+-- misc other builtin plugins {{{
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
 lvim.builtin.dashboard.active = true
 lvim.builtin.terminal.active = true
-lvim.builtin.nvimtree.side = "left"
-lvim.builtin.nvimtree.show_icons.git = 0
-
-lvim.builtin.bufferline.active = false
-
--- if you don't want all the parsers change this to a table of the ones you want
-lvim.builtin.treesitter.ensure_installed = {}
-lvim.builtin.treesitter.ignore_install = { "haskell" }
-lvim.builtin.treesitter.highlight.enable = true
 
 lvim.builtin.telescope.defaults.layout_config.prompt_position = "top"
 lvim.builtin.telescope.defaults.sorting_strategy = "ascending"
 
--- lvim.builtin.galaxyline.active = false
-lvim.builtin.lualine.options.theme = "tokyonight"
-lvim.builtin.lualine.options.section_separators = {'', ''}
-lvim.builtin.lualine.options.component_separators = {'', ''}
+lvim.builtin.lualine.style = "lvim"
+lvim.builtin.lualine.options.theme = "auto"
+lvim.builtin.lualine.options.section_separators = { "", "" }
+lvim.builtin.lualine.options.component_separators = { "", "" }
+lvim.builtin.lualine.options.disabled_filetypes = { "lua" }
+lvim.builtin.lualine.sections.lualine_a = { "mode" }
+lvim.builtin.lualine.extensions = { "nvim-tree" }
 
 lvim.builtin.gitsigns.opts.signs.add.text = "▌"
 lvim.builtin.gitsigns.opts.signs.change.text = "▌"
 lvim.builtin.gitsigns.opts.signs.changedelete.text = "▌"
 
+lvim.builtin.bufferline.active = false
+-- }}}
+
+-- NvimTree {{{
+local nvimtree = lvim.builtin.nvimtree
+nvimtree.side = "left"
+nvimtree.width = 45
+nvimtree.show_icons.git = 1
+nvimtree.show_icons = {
+  git = 1,
+  folders = 1,
+  files = 1,
+  folder_arrows = 0,
+}
+nvimtree.icons = {
+  default = "",
+  symlink = "",
+  git = {
+    unstaged = "✗",
+    staged = "✓",
+    unmerged = "",
+    renamed = "➜",
+    untracked = "★",
+    deleted = "",
+    ignored = "◌",
+  },
+  folder = {
+    -- arrow_open = "",
+    -- arrow_closed = "",
+    default = "",
+    open = "",
+    empty = "",
+    empty_open = "",
+    symlink = "",
+    symlink_open = "",
+  },
+  lsp = {
+    hint = "",
+    info = "",
+    warning = "",
+    error = "",
+  },
+}
+-- }}}
+
+-- treesitter {{{
+-- if you don't want all the parsers change this to a table of the ones you want
+lvim.builtin.treesitter.ensure_installed = {}
+lvim.builtin.treesitter.ignore_install = {} -- e.g. { "haskell" }
+lvim.builtin.treesitter.highlight.enable = true
+-- }}}
+
+-- telescope {{{
 require("telescope").setup({
   defaults = {
     -- Your defaults config goes in here
@@ -170,8 +271,10 @@ require("telescope").setup({
     -- Your extension config goes in here
   },
 })
+-- }}}
 
--- generic LSP settings
+-- LSP settings {{{
+-- generic settings
 -- disable highlighting reference under cursor
 lvim.lsp.document_highlight = false
 -- you can set a custom on_attach function that will be used for all the language servers
@@ -218,22 +321,10 @@ lvim.lang.scala.linters = {}
 --     args = {}
 --   }
 -- }
+-- }}}
 
--- Additional Plugins
+-- Additional Plugins {{{
 lvim.plugins = {
-  -- {
-  --   "NLKNguyen/papercolor-theme"
-  -- },
-  -- {
-  --   "projekt0n/github-nvim-theme",
-  -- config = function()
-  --   require("github-theme").setup({
-  --     themeStyle = "light",
-  --     functionStyle = "italic",
-  --     sidebars = {"qf", "vista_kind", "terminal", "packer"},
-  --   })
-  -- end
-  -- },
 
   {
     "tpope/vim-fugitive",
@@ -256,12 +347,16 @@ lvim.plugins = {
     ft = { "fugitive" },
   },
   {
+    "tpope/vim-surround",
+  },
+  {
     "folke/lsp-colors.nvim",
   },
   {
     "folke/trouble.nvim",
     cmd = "TroubleToggle",
   },
+
   {
     "norcalli/nvim-colorizer.lua",
     config = function()
@@ -277,9 +372,6 @@ lvim.plugins = {
     end,
   },
 
-  {
-    "~/workspace/neon",
-  },
   {
     "akinsho/bufferline.nvim",
     config = function()
@@ -313,15 +405,38 @@ lvim.plugins = {
     "rafcamlet/nvim-luapad",
   },
   {
+    "rktjmp/lush.nvim"
+  },
+
+  -- themes
+  {
     "Th3Whit3Wolf/space-nvim",
   },
   {
     "marko-cerovac/material.nvim",
   },
-
   {
     "folke/tokyonight.nvim",
   },
+  {
+    "NLKNguyen/papercolor-theme",
+  },
+  {
+    "rafamadriz/neon",
+  },
+  {
+    "fmcgough/pencil-color.nvim"
+  },
+  -- {
+  --   "projekt0n/github-nvim-theme",
+  -- config = function()
+  --   require("github-theme").setup({
+  --     themeStyle = "light",
+  --     functionStyle = "italic",
+  --     sidebars = {"qf", "vista_kind", "terminal", "packer"},
+  --   })
+  -- end
+  -- },
 
   -- {
   --     "ray-x/lsp_signature.nvim",
@@ -329,8 +444,12 @@ lvim.plugins = {
   --     event = "InsertEnter"
   -- }
 }
+-- }}}
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
 -- lvim.autocommands.custom_groups = {
 --   { "BufWinEnter", "*.lua", "setlocal ts=8 sw=8" },
 -- }
+
+-- Load the colorscheme at the end
+lvim.colorscheme = colorscheme
